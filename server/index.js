72e04f94180d906ead261131b4aa817f722df6c8
rupamc <rupam.chakraborty@exa-ag.com>
2023-01-03 21:20:46 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ const __dirname = path.dirname(__filename);
 
 /** **************CROS*************** */
 app.use(
-	cors({
-		origin: '*',
-	})
+  cors({
+    origin: '*',
+  }),
 );
 app.options('*', cors());
 
 /** **************DOTENV*************** */
 dotenv.config({
-	path: path.join(__dirname, '.env'),
+  path: path.join(__dirname, '.env'),
 });
 
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
@@ -31,16 +31,16 @@ app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000);
 
 app.use(bodyParser.json());
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  }),
 );
 // Serve our static build files
 // app.use(express.static(path.join(__dirname, './build')));
 
 app.get('/', (req, res, next) => {
-	//res.sendFile(path.join(__dirname, './build/index.html'));
-	res.send('Got it');
+  //res.sendFile(path.join(__dirname, './build/index.html'));
+  res.send('Got it');
 });
 
 /** **************DEFAULT API ENDPOINT*************** */
@@ -48,16 +48,16 @@ app.use('/api', router);
 
 /** **************DEVELOPMENT SERVER ERROR HANDLER*************** */
 if (process.env.NODE_ENV === 'development') {
-	app.use(errorHandler());
+  app.use(errorHandler());
 }
 
 app.listen(app.get('port'), () => {
-	console.log(
-		'Node.js App is running at http://localhost:%d',
-		app.get('port'),
-		app.get('env')
-	);
-	console.log('Press CTRL-C to stop\n');
+  console.log(
+    'Node.js App is running at http://localhost:%d',
+    app.get('port'),
+    app.get('env'),
+  );
+  console.log('Press CTRL-C to stop the server\n');
 });
 
 /** ************************************************************ */
@@ -66,7 +66,7 @@ app.listen(app.get('port'), () => {
 
 // Serving react on routes unused by previous routing
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, './build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 export default app;
